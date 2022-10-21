00e3854f455187889f02ff761a6fc15a902c0e66
hennyrose <93723100+hennyrose@users.noreply.github.com>
2022-10-21 20:10:33 +0300

    Update NEWfile_serviceMain.java

--- NEWfile_serviceMain.java
+++ NEWfile_serviceMain.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-
+// дублює файли окремого типу з однієї папки в іншу
 	public static void main(String[] args) {
 
 		File folderIn = new File("/Users/henny/Documents/folder1");
